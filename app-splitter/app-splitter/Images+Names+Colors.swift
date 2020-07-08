a787deebcfb14d485fa53f2f362f841c05d1cda3
mailys <mailys.perez@epitech.eu>
2020-07-08 09:46:47 +0200

    [ADD] static data

--- app-splitter/app-splitter/Images+Names+Colors.swift
+++ app-splitter/app-splitter/Images+Names+Colors.swift
@@ -11,7 +11,16 @@ import UIKit
 // Images
 let camera = UIImage(named: "1")!
 let city = UIImage(named: "2")!
-let buffalo = UIImage(named: "3")!
+let animal = UIImage(named: "3")!
 let bouquet = UIImage(named: "4")!
 let stand = UIImage(named: "5")!
 let urban = UIImage(named: "6")!
+
+let imagesArray = [camera, city, animal, bouquet, stand, urban]
+
+// Names
+let namesArray = ["CAMERA", "CITY", "ANIMAL", "BOUQUET", "STAND", "URBAN"]
+
+// Colors
+let colorsArray = [#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)]
+
