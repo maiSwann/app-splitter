511152e5c086c7a7700bfc250bb655d818d97c07
mailys <mailys.perez@epitech.eu>
2020-07-08 09:41:22 +0200

    [ADD] architecture and data file

---
+++ app-splitter/app-splitter/Images+Names+Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Images+Names+Colors.swift
+//  app-splitter
+//
+//  Created by Maïlys Perez on 08/07/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+// Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouquet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
